d846623ee550363c4595adf09297309f08346691
Otávio Monteiro Rossoni <70729846+Otarossoni@users.noreply.github.com>
2024-02-26 11:06:52 -0300

    feat: adiciona a extração do nome do medicamento de outro lugar

--- server/src/modules/promptuary/functions/findMaterialDescription.ts
+++ server/src/modules/promptuary/functions/findMaterialDescription.ts
@@ -1,6 +1,7 @@
 export function findMaterialDescription(fullResource) {
   if (fullResource.resource.resourceType === 'MedicationRequest') {
     const coding =
+      fullResource.resource.medicationCodeableConcept?.text ??
       fullResource.resource.medicationCodeableConcept?.coding?.[0]?.display ??
       null
 
